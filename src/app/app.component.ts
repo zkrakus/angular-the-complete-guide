1a3991e9a7825b0a646b2106f4965d1e0e2c7dec
zkrakus <zkrakus@gmail.com>
2025-03-24 22:22:54 -0400

    feat:user component using signals.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {}
