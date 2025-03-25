c9cac240c5c7fcb8128ec7147af0bba0160f5ec7
zkrakus <zkrakus@gmail.com>
2025-03-25 12:39:24 -0400

    feat:fixed signal code and added computed image path value.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,11 +7,14 @@ const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  avatar = input.required<string>();// Can also do input<string>(''). 
+  @Input({required: true}) name!: string;
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
 
   onSelectUser(){
-    const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+
   }
 }
