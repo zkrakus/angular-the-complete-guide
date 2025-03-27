108d0f0f37f06ccca44242dab851f93100dda40a
zkrakus <zkrakus@gmail.com>
2025-03-27 16:07:29 -0400

    feat:s2-56 Created new add task submission form and made it usable.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 
+import { type User } from './user.model';
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,19 +9,15 @@ import { Component, EventEmitter, Input, Output, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar! : string;
-  @Input({required: true}) name! : string;
-
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
-
-
 }
